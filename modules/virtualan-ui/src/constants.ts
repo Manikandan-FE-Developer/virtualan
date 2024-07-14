7926e87a0535d37a75c1e5e6514b2777573cf38c
Manikandan <manikandanbca87@gmail.com>
2024-07-14 16:41:58 +0530

    Deleted Virtualan UI

--- modules/virtualan-ui/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const API_GET_ENDPOINT_ADD = "/virtualservices/load";
-export const API_GET_ENDPOINT_LOAD = "/virtualservices";
-export const API_DELETE_ENDPOINT = "/virtualservices";
-export const API_POST_ENDPOINT = "/virtualservices";
-export const API_GET_CATALOGS = "/api-catalogs";
-export const VERSION = "v3.1.0";
-
-
--- modules/virtualan-ui/src/declarations.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'react-json-pretty/dist/monikai';
-declare module 'js-yaml';
